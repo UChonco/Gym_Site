e21e62628170d33f287c274e40494fca8903ee1f
UJ <uchonco21@gmail.com>
2024-09-10 23:50:44 +0200

    Updated

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
